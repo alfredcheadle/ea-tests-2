fdc078ae8c851b49ce887c16fe8bc3952ca56e2d
alfredcheadle <80414721+alfredcheadle@users.noreply.github.com>
2022-08-22 17:22:03 -0400

    The actual update

--- donatefooter.js
+++ donatefooter.js
@@ -1,7 +1,7 @@
 var myPostRender = function(vals) {
     (function ($) {
       console.log('TEST2 Branch served:');
-        document.querySelector('.at-markup.HeaderHtml').textContent('TEST2 Dev Branch Served');
+        document.querySelector('.at-markup.HeaderHtml').textContent = 'TEST2 Dev Branch Served';
         document.querySelector('.site-header').style.background = 'pink';
         
     }(jQuery));
